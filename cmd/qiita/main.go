edf7bb7166e1692cda94d92c123cbbdcb2dc2509
botchro <private.mnakamura@gmail.com>
2018-11-28 23:12:13 +0900

    [add] show options and show selected item body

--- cmd/qiita/main.go
+++ cmd/qiita/main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 
+	"github.com/manifoldco/promptui"
+
 	"github.com/botchro/qiita"
 )
 
@@ -13,16 +15,35 @@ func main() {
 		p = flag.Int("p", 1, "page number (default 1)")
 		n = flag.Int("n", 20, "number of items get once (default 20)")
 		q = flag.String("q", "", "search query string (default empty string)")
+		s = flag.Int("s", 20, "number of items show in one view")
 	)
 	flag.Parse()
 
+	// get first items
 	posts, err := qiita.GetItems(*p, *n, *q)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	for _, post := range posts {
-		fmt.Println(post.Title)
+	prompt := promptui.Select{
+		Label: "choose an article",
+		Items: posts,
+		Templates: &promptui.SelectTemplates{
+			Label:    "{{ . }}?",
+			Active:   "> {{ .Title | cyan }}",
+			Inactive: "  {{ .Title | cyan }}",
+			Selected: "{{ .Title | red | cyan }}",
+		},
+		Size: *s,
 	}
+
+	i, _, err := prompt.Run()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(posts[i].Body)
+
 }
